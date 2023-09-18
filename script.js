6c67a0805e39855a560b3c94453a44d8a0f7dc2b
Oshani Sharma <85727177+oshani-code@users.noreply.github.com>
2023-09-18 23:46:01 +0530

    Login Authentication Form

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-function convertTemperature() {
-    const tempInput = document.getElementById("tempInput").value;
-    const unitSelect = document.getElementById("unitSelect").value;
-    const resultElement = document.getElementById("result");
-    
-    if (unitSelect === "celsius") {
-        const fahrenheit = (tempInput * 9/5) + 32;
-        resultElement.innerHTML = `${tempInput}°C is ${fahrenheit.toFixed(2)}°F`;
-    } else if (unitSelect === "fahrenheit") {
-        const celsius = (tempInput - 32) * 5/9;
-        resultElement.innerHTML = `${tempInput}°F is ${celsius.toFixed(2)}°C`;
-    }
-}
+
+    // Simulating a user database
+    const users = [
+      { username: "admin", password: "password123" },
+      { username: "oshni", password: "pass123" },
+      { username: "krishna", password: "pass456" }
+    ];
+
+    // Function to handle form submission
+    $("#loginForm").submit(function(event) {
+      event.preventDefault(); // Prevent form from submitting
+
+      const username = $("#username").val();
+      const password = $("#password").val();
+
+      // Check if the entered username and password match any user in the database
+      const user = users.find(u => u.username === username && u.password === password);
+
+      if (user) {
+        // Store the username in localStorage
+        localStorage.setItem("username", username);
+
+        alert("Login successful! Thankyou for login ");
+        // Redirect to the home page or perform any other action
+      } 
+      else
+      {
+        alert("Invalid username or password. Please try again.");
+      }
+    });
+  
